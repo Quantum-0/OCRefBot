140c6bc1e7bd69010ce353069de28366351bbcec
Anton Kurenkov <quantum0@mail.ru>
2025-04-04 14:56:17 +0300

    WiP tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -36,3 +36,61 @@
 #   "text":"/start"
 # }
 # }' -H "X-Telegram-Bot-Api-Secret-Token: bnklgnbklgnfkbnkgfnbkfg" "https://ocrefbot.quantum0.ru/webhook"
+
+
+from unittest.mock import AsyncMock
+
+import pytest
+from aiogram import Bot, Dispatcher
+from aiogram.types import User
+from aiopg.sa import Engine
+from testcontainers.postgres import PostgresContainer
+
+from oc_ref_bot.bot import make_bot, make_dp
+from oc_ref_bot.config import settings
+from oc_ref_bot.database import db_engine
+
+pytestmark = pytest.mark.asyncio
+
+
+@pytest.fixture
+async def pg_engine() -> Engine:
+    with PostgresContainer('postgres:16-alpine', driver='psycopg2') as postgres:
+        settings.db_host = postgres.get_container_host_ip() + ':' + str(postgres.get_exposed_port(postgres.port))
+        settings.db_user = postgres.username
+        settings.db_pass = postgres.password
+        settings.db_db = postgres.dbname
+        async with db_engine() as engine:
+            yield engine
+
+
+@pytest.fixture
+async def bot() -> Bot:
+    """Creates a bot instance with a mocked send_message function."""
+    Bot.set_my_commands = AsyncMock()
+    Bot.get_me = AsyncMock(
+        return_value=User(
+            id=111222333,
+            is_bot=True,
+            first_name='OC Reference Bot [0.0.1]',
+            last_name=None,
+            username='ocrefbot',
+        )
+    )
+    Bot.set_my_name = AsyncMock()
+    Bot.delete_webhook = AsyncMock()
+    bot = await make_bot()
+    bot.set_my_name.assert_called_once()
+    bot.delete_webhook.assert_called_once()
+    Bot.send_message = AsyncMock()
+    Bot.send_sticker = AsyncMock()
+
+    return bot
+
+
+@pytest.fixture
+async def dispatcher(pg_engine: Engine) -> Dispatcher:
+    """Creates a Dispatcher instance."""
+    dp = await make_dp(pg_engine)
+    await dp.emit_startup(pg=pg_engine)
+    return dp
--- tests/test_test.py
+++ tests/test_test.py
@@ -1,57 +1,11 @@
-from datetime import datetime
-from unittest.mock import AsyncMock
+from datetime import UTC, datetime
 
 import pytest
-from aiogram import Bot, Dispatcher
 from aiogram.types import Message, Update
-from testcontainers.postgres import PostgresContainer
 
-from oc_ref_bot.bot import UsersMiddleware
-from oc_ref_bot.cmd_router import router as cmd_router
-from oc_ref_bot.config import settings
-from oc_ref_bot.database import create_tables, db_engine, get_user
+from oc_ref_bot.database import get_user
 
-pytestmark = pytest.mark.asyncio  # Required for pytest-asyncio
-
-
-@pytest.fixture
-async def pg_conn():
-    with PostgresContainer('postgres:16-alpine', driver='psycopg2') as postgres:
-        # psql_url = postgres.get_connection_url(driver='psycopg2')
-        settings.db_host = postgres.get_container_host_ip() + ':' + str(postgres.get_exposed_port(postgres.port))
-        settings.db_user = postgres.username
-        settings.db_pass = postgres.password
-        settings.db_db = postgres.dbname
-        async with db_engine() as engine:
-            yield engine
-            # async with engine.acquire() as conn:
-            #     await create_tables(conn)
-            #     yield conn
-        # async with db_engine() as connection:
-        # engine = aiopg.sa.create_engine(psql_url)
-        # async with engine as connection:
-        #     await create_tables(connection)
-        #     yield connection
-
-
-@pytest.fixture
-async def bot():
-    """Creates a bot instance with a mocked send_message function."""
-    bot = Bot(token=settings.bot_token)
-    bot.send_message = AsyncMock()  # Mock sending messages to avoid real API calls
-    bot.send_sticker = AsyncMock()  # Mock sending messages to avoid real API calls
-
-    return bot
-
-
-@pytest.fixture
-async def dispatcher(bot, pg_conn):
-    """Creates a Dispatcher instance."""
-    dp = Dispatcher(pg=pg_conn)
-    dp._bot = bot  # Attach the mock bot to the dispatcher
-    dp.message.middleware(UsersMiddleware())
-    dp.include_router(cmd_router)
-    return dp
+pytestmark = pytest.mark.asyncio
 
 
 @pytest.fixture
@@ -62,17 +16,14 @@ async def message(bot):
         from_user={'id': 123456789, 'is_bot': False, 'first_name': 'TestUser'},
         chat={'id': 123456789, 'type': 'private'},
         text='/start',
-        date=datetime.now(),
+        date=datetime.now(tz=UTC),
     )
     msg._bot = bot
     return msg
 
 
-async def test_start_command(bot, dispatcher, message, pg_conn):
+async def test_start_command(bot, dispatcher, message, pg_engine):
     """Test if the bot correctly handles the /start command."""
-    # await cmd_start(message)  # Simulate handling the command
-    async with pg_conn.acquire() as conn:
-        await create_tables(conn)
 
     await dispatcher.feed_update(bot, Update(update_id=1234, message=message))
 
@@ -85,51 +36,7 @@ async def test_start_command(bot, dispatcher, message, pg_conn):
         message.chat.id,
         'CAACAgIAAxkBAT2NsWbDhsizAAFqJIcz1hZsDkrJm1UqfQACtUwAAiNsuErWfaHtCbzGbDUE',
     )
-    async with pg_conn.acquire() as conn:
+    async with pg_engine.acquire() as conn:
         user = await get_user(conn, message.from_user.id)
         assert user
         assert 1
-
-
-# import json
-#
-# import pytest
-# from aiogram.types import Update
-# from aiohttp import web
-# from oc_ref_bot.main import main_bot  # Import your bot startup function
-#
-#
-# @pytest.fixture
-# async def test_client(aiohttp_client):
-#     """Starts the bot's webhook server and returns a test client."""
-#     app = web.Application()
-#
-#     # Start the bot server
-#     await main_bot()  # Ensure your bot registers webhook handlers
-#
-#     # Create a test client
-#     return await aiohttp_client(app)
-#
-# async def test_webhook_start_command(test_client):
-#     '''Sends a fake /start command via webhook and checks response.'''
-#
-#     # Simulated Telegram webhook payload
-#     fake_update = Update(
-#         update_id=1,
-#         message={
-#             'message_id': 1,
-#             'from': {'id': 123456789, 'is_bot': False, 'first_name': 'TestUser'},
-#             'chat': {'id': 123456789, 'type': 'private'},
-#             'text': '/start',
-#         },
-#     ).model_dump_json()
-#
-#     # Send webhook request to your bot
-#     response = await test_client.post('/your-webhook-path', data=json.dumps(fake_update))
-#
-#     # Assert response is 200 OK (Telegram expects this)
-#     assert response.status == 200
-#
-#     # Optionally, check if the bot's response contains expected text
-#     response_text = await response.text()
-#     assert 'Hello! Welcome to the bot' in response_text
